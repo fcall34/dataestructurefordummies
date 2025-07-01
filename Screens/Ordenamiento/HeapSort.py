3a234b12c02e18e81d4b1a50c517d4a97ed4a582
PunKas1 <2004andyflorese@gmail.com>
2025-06-30 22:00:43 -0600

    Heap y Merge

---
+++ Screens/Ordenamiento/HeapSort.py
@@ -0,0 +1,27 @@
+from UIS.HeapSort_ui import Ui_StackedWidget
+from PySide6.QtWidgets import QStackedWidget
+
+class VentanaHeapSort(QStackedWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui=Ui_StackedWidget()
+        self.ui.setupUi(self)
+
+        self.ui.btnNext.clicked.connect(self.nextPage)
+        self.ui.btnNext_2.clicked.connect(self.nextPage)
+        self.ui.btnNext_3.clicked.connect(self.nextPage)
+        self.ui.btnNext_4.clicked.connect(self.nextPage)
+        self.ui.btnNext_5.clicked.connect(self.nextPage)
+        self.ui.btnTermiando.clicked.connect(self.finish)
+    
+
+    def nextPage(self):
+        current_page = self.currentIndex()
+        total_pages = self.count()
+
+        if current_page < total_pages - 1:
+            self.setCurrentIndex(current_page+1)
+
+    def finish(self):
+        self.hide()
+        
--- Screens/Ordenamiento/menuOrdenamiento.py
+++ Screens/Ordenamiento/menuOrdenamiento.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QMainWindow
 from Screens.Ordenamiento.Quicksort import VentanaQuickSort
+from Screens.Ordenamiento.MergeSort import VentanaMergeSort
 from UIS.Ordenamiento_ui import Ui_MainWindow
+from Screens.Ordenamiento.HeapSort import VentanaHeapSort
 
 class VentanaOrdenamiento(QMainWindow):
     def __init__(self):
@@ -9,8 +11,22 @@ class VentanaOrdenamiento(QMainWindow):
         self.ui.setupUi(self)
 
         self.ui.QUICKSORT.clicked.connect(self.abrir_QuickSort)
+        self.ui.MERGESORT.clicked.connect(self.abrir_MergeSort)
+        self.ui.HEAPSORT.clicked.connect(self.abrir_HeapSort)
+        self.ui.ATRAS.clicked.connect(self.finish)
         
     def abrir_QuickSort(self):
         self.ventanaQuick = VentanaQuickSort()
         self.ventanaQuick.show()
+
+    def abrir_MergeSort(self):
+        self.ventanaMerge = VentanaMergeSort()
+        self.ventanaMerge.show()
+
+    def abrir_HeapSort(self):
+        self.ventanaHeap = VentanaHeapSort()
+        self.ventanaHeap.show()
+
+    def finish(self):
+        self.hide()
     
