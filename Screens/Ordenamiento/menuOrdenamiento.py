a76a0a5f369d0e28f381730e2973980d431d30d1
PunKas1 <2004andyflorese@gmail.com>
2025-07-01 22:15:29 -0600

    Ordenamiento Completo

--- Screens/Ordenamiento/menuOrdenamiento.py
+++ Screens/Ordenamiento/menuOrdenamiento.py
@@ -1,8 +1,9 @@
 from PySide6.QtWidgets import QMainWindow
 from Screens.Ordenamiento.Quicksort import VentanaQuickSort
 from Screens.Ordenamiento.MergeSort import VentanaMergeSort
-from UIS.Ordenamiento_ui import Ui_MainWindow
 from Screens.Ordenamiento.HeapSort import VentanaHeapSort
+from Screens.Ordenamiento.CountingSort import VentanaCountingSort
+from UIS.Ordenamiento_ui import Ui_MainWindow
 
 class VentanaOrdenamiento(QMainWindow):
     def __init__(self):
@@ -13,7 +14,9 @@ class VentanaOrdenamiento(QMainWindow):
         self.ui.QUICKSORT.clicked.connect(self.abrir_QuickSort)
         self.ui.MERGESORT.clicked.connect(self.abrir_MergeSort)
         self.ui.HEAPSORT.clicked.connect(self.abrir_HeapSort)
+        self.ui.COUNTINGSORT.clicked.connect(self.abrir_Counting)
         self.ui.ATRAS.clicked.connect(self.finish)
+       
         
     def abrir_QuickSort(self):
         self.ventanaQuick = VentanaQuickSort()
@@ -26,6 +29,10 @@ class VentanaOrdenamiento(QMainWindow):
     def abrir_HeapSort(self):
         self.ventanaHeap = VentanaHeapSort()
         self.ventanaHeap.show()
+    
+    def abrir_Counting(self):
+        self.ventanaCount = VentanaCountingSort()
+        self.ventanaCount.show()
 
     def finish(self):
         self.hide()
